fb57401fbd4f39fd280fd30a2c0857a0d73b70c1
alfredlucero <alfred.lucero@ucla.edu>
2020-04-16 14:21:45 -0700

    Added functions.py

---
+++ functions.py
@@ -0,0 +1,9 @@
+# python functions.py
+def say_hello(first_name, last_name):
+    """Say hello."""
+    print("Hello, I am {0} {1}".format(first_name, last_name))
+
+
+say_hello("alfred", "lucero")
+say_hello(last_name="deguzman", first_name="gingin")
+help(say_hello)
