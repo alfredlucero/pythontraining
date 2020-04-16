fe1c819ad4d0dd23d50b4400d076a7a8d2054629
alfredlucero <alfred.lucero@ucla.edu>
2020-04-16 13:30:36 -0700

    Added numbers.py

---
+++ numbers.py
@@ -0,0 +1,29 @@
+# python numbers.py
+# "int"
+integer = 42
+print("Integer:", integer)
+# "float"
+float_num = 4.2
+print("Float:", float_num)
+# Integer + Float => Float
+
+# + add
+# - subtract
+# * multiply
+# / divide
+# ** exponentiate
+# % modulo (remainder)
+
+# int()
+quantity_string = '3'
+total = int(quantity_string) + 2
+print("Total Int:", total)
+# float()
+quantity_string = '3'
+total = float(quantity_string) + 2
+print("Total Float:", total)
+
+""" 
+  Multi-line comment
+"""
+# Single line comment
