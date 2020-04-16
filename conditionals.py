25a51703db92a1129fa97148e7cc73df62ce87e9
alfredlucero <alfred.lucero@ucla.edu>
2020-04-16 14:16:37 -0700

    Added conditionals.py and actually ignored pycache file

---
+++ conditionals.py
@@ -0,0 +1,24 @@
+# python conditionals.py
+boolean_true = True
+print(boolean_true)
+boolean_false = False
+print(boolean_false)
+
+# ==
+# >
+# >=
+# <
+# <=
+# !=
+
+input_age = int(input("How old are you?"))
+if input_age > 25:
+    print("You are too old!")
+else:
+    print("Too young!")
+
+
+# Operators
+# and
+# or
+# not
